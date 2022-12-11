bc86bdec7b433346ac91df5a97d02c23d6eb8f18
MarynaSkoryk <108087570+MarynaSkoryk@users.noreply.github.com>
2022-12-11 16:19:13 +0000

    Deploying to gh-pages from @ MarynaSkoryk/hex-colors@72e354b37249e95521f18714c7dbab19c8e886e6 🚀

---
+++ index.6c5e951b.js
@@ -0,0 +1,2 @@
+function changeColor(){for(var e=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"],o="",n=0;n<6;n+=1){o+=e[Math.floor(Math.random()*e.length)]}document.getElementById("hex-code").innerHTML=o,document.getElementsByTagName("body")[0].style.background="#"+o}changeColor();
+//# sourceMappingURL=index.6c5e951b.js.map
---
+++ index.a2067ccb.js
@@ -0,0 +1,2 @@
+!function(){for(var e=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"],n="",t=0;t<6;t+=1)n+=e[Math.floor(Math.random()*e.length)];document.getElementById("hex-code").innerHTML=n,document.getElementsByTagName("body")[0].style.background="#"+n}();
+//# sourceMappingURL=index.a2067ccb.js.map
